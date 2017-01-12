88f1835e068b2c8611ff5ed7e490f6020a707599
mulinrui <mulinrui@163.com>
2017-01-12 13:13:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hanfl/ninetempledemo/MainActivity.java
@@ -0,0 +1,220 @@
+package com.example.hanfl.ninetempledemo;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.example.hanfl.ninetempledemo.adapter.DemoAdapter;
+import com.example.hanfl.ninetempledemo.data.PrizeData;
+import com.example.hanfl.ninetempledemo.eventbus.MessageEvent;
+import com.example.hanfl.ninetempledemo.utils.AppJsonFileReader;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.x_recycler_view)
+    RecyclerView xRecyclerView;
+    @BindView(R.id.x_recycler_view2)
+    RecyclerView xRecyclerView2;
+    @BindView(R.id.x_recycler_view3)
+    RecyclerView xRecyclerView3;
+    DemoAdapter demoAdapter;
+    List<PrizeData> prizeList = new ArrayList<>();// adapter 数据源
+    private MainHandler mainHandler = new MainHandler(new WeakReference<>(this));
+
+    private volatile boolean isRun = true;//点击后启动抽奖线程，循环的标志，当奖品开出后，并旋转到奖品位置时停止线程。
+    private volatile int count = 0;//旋转计数器，每跳一格+1,然后取余8得到array数组的下标，
+    private int[] array = new int[]{0, 1, 2, 5, 8, 7, 6, 3};//顺时针旋转数组，按照这个数组的排列顺序来改变方块背景色表示正在抽奖
+    private int result = 9;//开奖结果，当数字属于0-8时表示已经开奖，此时要停止线程
+    private boolean isResult = true;// 是否生成结果，（正式使用时可以是判断是否去请求开奖结果的标志）
+    AlertDialog.Builder builder;//提示奖品的dialog
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        xRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        demoAdapter = new DemoAdapter(this, prizeList);
+        xRecyclerView.setAdapter(demoAdapter);
+
+        xRecyclerView2.setLayoutManager(new GridLayoutManager(this, 3));
+        xRecyclerView2.setAdapter(demoAdapter);
+
+        xRecyclerView3.setLayoutManager(new GridLayoutManager(this, 3));
+        xRecyclerView3.setAdapter(demoAdapter);
+
+        getJsonData();//获取所有奖品数据
+        EventBus.getDefault().register(this);
+        builder = new AlertDialog.Builder(this);
+    }
+
+    /**
+     * 获取数据源
+     */
+    private void getJsonData() {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                String jsonStr = AppJsonFileReader.getJson(getBaseContext(), "result.json");
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonStr);
+                    String data = jsonObject.getString("data");
+                    Gson gson = new Gson();
+                    List<PrizeData> list = gson.fromJson(data, new TypeToken<List<PrizeData>>() {
+                    }.getType());
+                    Message msg = new Message();
+                    msg.what = 2;
+                    msg.obj = list;
+                    mainHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe
+    public void onMessageEvent(MessageEvent event) {
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                Log.d("refreshView---->", "refreshView");
+                refreshView();
+            }
+        }.start();
+    }
+
+    private synchronized void refreshView() {
+        try {
+            while (isRun) {
+                Thread.sleep(200);
+                int m = count % 8;
+                Log.d("refreshView---->", "m:" + m + " result:" + result);
+                if (m == 0) {
+                    prizeList.get(array[array.length - 1]).img = null;
+                } else {
+                    prizeList.get(array[m - 1]).img = null;
+                }
+                prizeList.get(array[m]).img = "2";
+                mainHandler.sendEmptyMessage(1);
+                count++;
+                if (m == result) {
+                    isRun = false;
+                    mainHandler.sendEmptyMessage(3);
+                }
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 写了一个生成随机数的方法，生成0-7的随机数
+     * <p>
+     * 在使用时可替换为获取开奖结果的方法，然后延迟几秒给result赋值
+     */
+    private void toLoginActivity() {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Random r = new Random();
+                result = r.nextInt(7);
+                Log.d("开奖结果---->", result + "");
+            }
+        };
+        timer.schedule(task, 1000 * 8);
+    }
+
+    private void getResultData() {
+
+    }
+
+    private static class MainHandler extends Handler {
+        private WeakReference<MainActivity> weakReference;
+
+        protected MainHandler(WeakReference<MainActivity> wk) {
+            weakReference = wk;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            final MainActivity activity = weakReference.get();
+            if (activity == null) {
+                return;
+            }
+            Log.d("MainHandler---->", "what:" + msg.what);
+            if (msg.what == 1) {
+                if (activity.isResult) {
+                    //获取抽奖结果，并关闭该判断，防止重复获取数据
+                    activity.toLoginActivity();
+                    activity.isResult = false;
+
+                }
+                activity.demoAdapter.notifyDataSetChanged();
+            }
+            if (msg.what == 2) {
+                List<PrizeData> list = (List<PrizeData>) msg.obj;
+                activity.prizeList.clear();
+                for (int i = 0; i < list.size(); i++) {
+                    if (i == 4) {
+                        PrizeData prizeData = new PrizeData();
+                        prizeData.id = UUID.randomUUID().toString();
+                        activity.prizeList.add(prizeData);
+                    }
+                    activity.prizeList.add(list.get(i));
+                }
+            }
+            if (msg.what == 3) {
+                activity.builder.setMessage(activity.prizeList.get(activity.array[activity.result]).name);
+                activity.builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //设置运行停止
+                        activity.demoAdapter.setRun(false);
+                        activity.isRun = true;
+                        activity.result = 9;
+                        activity.isResult = true;
+                        activity.demoAdapter.notifyDataSetChanged();
+                    }
+                });
+                activity.builder.show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hanfl/ninetempledemo/adapter/DemoAdapter.java
@@ -0,0 +1,81 @@
+package com.example.hanfl.ninetempledemo.adapter;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hanfl.ninetempledemo.R;
+import com.example.hanfl.ninetempledemo.data.PrizeData;
+import com.example.hanfl.ninetempledemo.eventbus.MessageEvent;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
+
+/**
+ * Created by HanFL on 2016/9/27.
+ * DemoAdapter34
+ */
+
+public class DemoAdapter extends RecyclerView.Adapter<DemoViewHolder> {
+    private Context mContext;
+    private List<PrizeData> list;
+    private boolean isRun = false;
+
+    public DemoAdapter(Context context, List<PrizeData> datas) {
+        this.mContext = context;
+        this.list = datas;
+    }
+
+    public void setDatas(List<PrizeData> mDatas) {
+        this.list = mDatas;
+    }
+
+    @Override
+    public DemoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        mContext = parent.getContext();
+        View view = LayoutInflater.from(mContext).inflate(R.layout.activity_main_item, parent, false);
+        return new DemoViewHolder(view);
+    }
+
+    public void setRun(boolean run) {
+        isRun = run;
+    }
+
+    @Override
+    public void onBindViewHolder(final DemoViewHolder holder, int position) {
+        if (position == 4) {
+            if (!isRun) {
+                holder.textView.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_default_award));
+                holder.textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EventBus.getDefault().post(new MessageEvent("Hello", "everyone!"));
+                        holder.textView.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_await_award));
+                        isRun = true;
+                        holder.textView.setOnClickListener(null);
+                    }
+                });
+            } else {
+                holder.textView.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_await_award));
+            }
+            holder.textView.setText("");
+        } else {
+            if (list.get(position).img == null) {
+                holder.textView.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_default));
+            } else {
+                holder.textView.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_select));
+            }
+            holder.textView.setText(list.get(position).name);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
